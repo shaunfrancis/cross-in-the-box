c549746b7bc36d230338706658b01211ada3e2e3
shaunfrancis <shaunfrancis@gmx.com>
2025-08-27 12:35:47 +0100

    Delete Next

--- src/app/uk/scottish-parliament-elections/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ScottishParliamentElections(){
-    return ( <>
-        <section>
-            <h1>Scottish Parliament Elections</h1>
-        </section>
-    </> )
-}
--- src/components/shared/MapCardGroup/MapCardGroup.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './MapCardGroup.module.css';
-
-export default function MapCardGroup({children} : {children : React.ReactNode}){
-    return (
-        <section className={styles['group']}>
-            {children}
-        </section>
-    )
-}
