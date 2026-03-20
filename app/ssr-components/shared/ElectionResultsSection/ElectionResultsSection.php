d3c4fc81f756418b4b1771b0444daeeca9c020be
shaunfrancis <shaunfrancis@gmx.com>
2026-03-20 18:39:24 +0000

    Add horizontal scrollbar for election results sections

--- app/ssr-components/shared/ElectionResultsSection/ElectionResultsSection.php
+++ app/ssr-components/shared/ElectionResultsSection/ElectionResultsSection.php
@@ -2,8 +2,11 @@
 namespace Shared;
 
 class ElectionResultsSection extends \Base\Component{
-    static function renderOpen(): void { ?>
-        <div class="ElectionResultsSection">
+    static function renderOpen(): void { 
+        $scrollerId = uniqid();
+    ?>
+        <?= HorizontalScroller::show(id: $scrollerId); ?>
+        <div class="ElectionResultsSection" data-scroller="<?= $scrollerId; ?>">
     <?php }
     static function renderClose(): void { echo '</div>'; }
 }
